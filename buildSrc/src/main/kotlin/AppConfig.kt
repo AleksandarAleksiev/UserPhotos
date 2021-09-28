ba795df76e5a8af0c29bdc7cf320dab448a3aa6a
Aleksandar Aleksiev <aleksandar.aleksiev@tui.co.uk>
2021-09-28 10:58:34 +0100

    [task] buildSrc folder & dependencies

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,5 @@
+object AppConfig {
+    const val targetSdk = 30
+    const val minSdk = 24
+    const val buildToolsVersion = "30.0.2"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,44 @@
+object Versions {
+    object Plugins {
+        const val agpVersion = "7.0.1"
+    }
+
+    object Kotlin {
+        const val kotlinVersion = "1.5.30"
+    }
+
+    object AndroidX {
+        const val coreKtxVersion = "1.5.0"
+        const val constraintLayoutVersion = "2.0.4"
+        const val hiltVersion = "2.38.1"
+        const val fragment = "1.3.6"
+        const val navigation = "2.3.5"
+        const val lifeCycle = "2.3.1"
+    }
+
+    object MaterialDesign {
+        const val materialVersion = "1.3.0"
+    }
+
+    object Network {
+        const val retrofit = "2.9.0"
+        const val okhttp = "4.9.1"
+    }
+
+    object Json {
+        const val moshi = "1.12.0"
+    }
+
+    object RxJava {
+        const val rxJava = "3.1.1"
+    }
+
+    object Test {
+        const val junit = "4.12"
+        const val mockito = "3.9.0"
+        const val androidXTest = "1.4.0"
+        const val androidXjUnit = "1.1.3"
+        const val androidXArchCore = "2.1.0"
+        const val espressoCore = "3.3.0"
+    }
+}
